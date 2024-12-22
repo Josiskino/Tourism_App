b0ee30df5099775e441a649b5117c6cd5997d7d0
Josiskino <lasselus15@gmail.com>
2024-12-22 19:35:24 +0000

    change ngrok url by Hostinger url

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,7 +1,8 @@
 class ApiConstant {
   ApiConstant._();
 
-  static const host = "https://09df-2c0f-f0f8-816-5c00-3019-1a4a-5759-dc0d.ngrok-free.app";
+  static const ngrokHost = "https://09df-2c0f-f0f8-816-5c00-3019-1a4a-5759-dc0d.ngrok-free.app";
+  static const host = "https://vakpo-api.rentxcel-app.com/";
   static const baseUrl = "$host/api/V1/";
   
 }
